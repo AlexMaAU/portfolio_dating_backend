cff4add190f96dd06f94fa078f08b148beccd115
AlexmaAU <alexma8809@gmail.com>
2024-05-01 13:11:11 +1000

    update admin validate logic

--- src/validations/adminValidate.ts
+++ src/validations/adminValidate.ts
@@ -3,7 +3,7 @@ import joi from 'joi';
 export const newAdminSchemaValidate = joi.object({
   username: joi.string().optional(),
   email: joi.string().email().required(),
-  password: joi.string().min(4).max(10).optional(),
+  password: joi.string().min(4).max(10).required(),
 });
 
 export const updateAdminSchemaValidate = joi.object({
